e2617ac3e41f82be799f231b7935ff9d0f8a3327
cristinamansi <cristinamansi.cm@gmail.com>
2024-03-21 16:29:34 +0100

    initial commit

---
+++ traveleasy-backend/src/main/java/com/skysavvy/traveleasy/TraveleasyApplication.java
@@ -0,0 +1,13 @@
+package com.skysavvy.traveleasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveleasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveleasyApplication.class, args);
+	}
+
+}
---
+++ traveleasy-backend/src/test/java/com/skysavvy/traveleasy/TraveleasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skysavvy.traveleasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraveleasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
