bf54331c4d74068c39332049821aaf4e7a9587f4
crimansi <cristinamansi.cm@gmail.com>
2024-10-09 21:14:43 +0200

    Test for Spring Data Rest

--- src/test/java/com/skysavvy/traveleasy/reporitory/BookedRepositoryUnitTests.java
+++ src/test/java/com/skysavvy/traveleasy/reporitory/BookedRepositoryUnitTests.java
@@ -8,19 +8,38 @@ import com.skysavvy.traveleasy.model.bookingFlight.Flight;
 import com.skysavvy.traveleasy.model.bookingFlight.TravelerEntity;
 import com.skysavvy.traveleasy.model.user.Role;
 import com.skysavvy.traveleasy.model.user.User;
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TraveleasyApplication.class)
+@AutoConfigureMockMvc
 public class BookedRepositoryUnitTests {
 
     @Mock
@@ -35,9 +54,12 @@ public class BookedRepositoryUnitTests {
     private Flight flight1;
     private TravelerEntity traveler;
     private TravelerEntity traveler1;
+    @Autowired
+    private MockMvc mockMvc;
 
     @BeforeEach
     public void setUp() {
+        MockitoAnnotations.openMocks(this);
         // Inizializzazione dell'utente
         user = new User("firstname", "lastname", "username", "user@example.com", "password123", Role.USER);
         user.setId(10L);
@@ -93,7 +115,29 @@ public class BookedRepositoryUnitTests {
     }
 
     @Test
-    public void testFindAllByUser(){
-        given(bookedRepository.findAllByUser())
+    public void testFindAllByUser() throws Exception {
+        Sort sort = Sort.by(Sort.Direction.ASC, "date");
+        List<Booked> bookeds = new ArrayList<>();
+        bookeds.add(booked);
+        bookeds.add(booked1);
+
+        when(bookedRepository.findAllByUser(user.getId(), sort)).thenReturn(Arrays.asList(booked, booked1));
+
+        List<Booked> response = bookedRepository.findAllByUser(user.getId(), sort);
+        System.out.println("Response from mock: " + response);
+        System.out.println("Booked 1: " + booked);
+        System.out.println("Booked 2: " + booked1);
+
+
+        mockMvc.perform(get("/bookings/search/findAllByUser?user="+user.getId()+"&sort=date,asc"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._embedded.bookings", hasSize(2))) // Verifica che ci siano 2 prenotazioni
+                .andExpect(jsonPath("$._embedded.bookings[0].date").value(booked.getDate().toString())) // Verifica la data della prima prenotazione
+                .andExpect(jsonPath("$._embedded.bookings[1].date").value(booked1.getDate().toString())) // Verifica la data della seconda prenotazione
+                .andExpect(jsonPath("$._embedded.bookings[0].booking.bookingId").value("BKG123")) // Assicurati di avere l'ID della prenotazione
+                .andExpect(jsonPath("$._embedded.bookings[0].booking.travelers[0].firstName").value("John")) // Verifica il nome del primo viaggiatore
+                .andExpect(jsonPath("$._embedded.bookings[0].booking.flight.cityDeparture").value("Milan")); // Verifica la città di partenza
     }
-}
+
+    }
+
