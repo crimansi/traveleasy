78443a7a4f1d844e3605b44731bb8b50477e342c
crimansi <128234511+crimansi@users.noreply.github.com>
2024-12-05 12:35:11 +0100

    Update BookingController.java

--- src/main/java/com/skysavvy/traveleasy/controller/BookingController.java
+++ src/main/java/com/skysavvy/traveleasy/controller/BookingController.java
@@ -19,26 +19,10 @@ public class BookingController {
     @Autowired
     BookingService bookingService;
 
-
-   /*@Operation(summary = "Get a list of flight bookings", description = "Return a list of Booked")
-    @GetMapping
-    public List<Booked> getBookings(@RequestHeader("Authorization") String tokenUser,
-                                    @Parameter(description = "Order of bookings", example = "ASC", required = true) @RequestParam String sort) throws ResponseException {
-        return bookingService. getAllPublicBookings(tokenUser, sort);
-    }*/
-
     @DeleteMapping("/{id}")
     public void deleteBooking(@RequestHeader("Authorization") String tokenUser,
                               @PathVariable(value = "id") Long bookingId) throws ResponseException {
         bookingService.deleteBooking(bookingId);
     }
-
-    /*@GetMapping("/range")
-    public List <Booked> getBookingsInRange(@RequestHeader("Authorization") String tokenUser,
-                                            @RequestParam String range,
-                                            @RequestParam String sort) throws ResponseException {
-        return bookingService.getBookingsFromRange(tokenUser, range, sort);
-    }*/
-
 
 }
